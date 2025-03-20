8346770e82055ed1438491348cf99c2660a14f5b
Justin Dennison <justin1dennison@gmail.com>
2025-03-19 20:03:33 -0400

    feature: create helper script to create a new journal entry

---
+++ bin/new-journal-entry.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Creating a new journal entry..."
+dt=$(date '+%F')
+filename="${dt}.md"
+path="./src/content/journal/${filename}"
+template=$(cat <<TEMPLATE
+---
+title: ${dt}
+tags: []
+draft: true
+---
+TEMPLATE
+)
+
+echo "${template}" > "${path}"
